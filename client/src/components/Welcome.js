851601361f0b53d8273fd37c89460ea39ce3f213
ZekeriyaOtumlu <zekyotumlu@hotmail.com>
2023-01-24 19:47:27 -0500

    Welcome page update

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Welcome() {
-    return (
-        <div className="home container">
-            <h1 className="">Welcome to the Recipes</h1>
-            <Link to='/signup' className="btn btn-primary btn-lg">Get Started</Link>
-        </div>
+    return ( <
+        div className = "home container" >
+        <
+        h1 className = "" > Welcome to the MDDZ Team Recipes < /h1> <
+        Link to = '/signup'
+        className = "btn btn-primary btn-lg" > Let 's Get Started</Link> <
+        /div>
     )
 }
 
